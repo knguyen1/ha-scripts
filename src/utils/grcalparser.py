97191d8a335af4cce5a5d8e7b65a5d9d9a7bdae5
Khoa Nguyen <yurameki@gmail.com>
2023-01-20 12:17:08 +0000

    Add 'get year'

--- src/utils/grcalparser.py
+++ src/utils/grcalparser.py
@@ -1,6 +1,6 @@
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 from datetime import date, timedelta
-from typing import Callable, Dict, List, Optional
+from typing import Callable, DefaultDict, Dict, List, Optional
 
 import requests
 from dateutil import parser
@@ -12,50 +12,57 @@ class GlenRockNjCalendarParser:
 
     def __init__(
         self,
-        start_date: date,
-        end_date: date,
-        item_id: int = 965,
-        limit: int = 0,
         session: Optional[requests.Session] = None,
-        area_filter: Optional[List[str]] = None,
+        item_title_filter: Optional[List[str]] = None,
     ) -> None:
-        self.start_date = start_date
-        self.end_date = end_date
-        self.item_id = item_id
-        self.limit = limit
-
-        self.area_filter = area_filter
+        self.item_title_filter = item_title_filter
 
         self.headers = {
             "Content-Type": "application/json",
             "Accept": "application/json",
         }
 
+        self.query_params = dict(option="com_dpcalendar", view="events", format="raw")
+
         self.session: requests.Session = session or requests.Session()
         self.base_api = "https://glenrocknj.net/index.php"
 
-    def get_items(self) -> OrderedDict:
-        query_params: Dict[str, str] = {}
-        query_params["option"] = "com_dpcalendar"
-        query_params["view"] = "events"
-        query_params["format"] = "raw"
-        query_params["limit"] = str(self.limit)
-        query_params["ItemId"] = str(self.item_id)
-        query_params["date-start"] = self.start_date.strftime("%Y-%m-%dT%H:%M:%S")
-        query_params["date-end"] = self.end_date.strftime("%Y-%m-%dT%H:%M:%S")
+    def get_items(
+        self, start_date: date, end_date: date, item_id: int, limit: int
+    ) -> OrderedDict:
+        query_params: Dict[str, str] = self.query_params.copy()
+        query_params["limit"] = str(limit)
+        query_params["ItemId"] = str(item_id)
+        query_params["date-start"] = start_date.strftime("%Y-%m-%dT%H:%M:%S")
+        query_params["date-end"] = end_date.strftime("%Y-%m-%dT%H:%M:%S")
 
         result = self._call_api("GET", self.base_api, params=query_params)
         result_json = self._parse_result_to_json(result)
         return result_json
 
-    def get_todays_items(self) -> List[str]:
-        return self._get_the_days_items(date.today())
+    def get_todays_items(self, item_id: int = 965, limit: int = 0) -> List[str]:
+        return self._get_the_days_items(date.today(), item_id, limit)
 
-    def get_tomorrows_items(self) -> List[str]:
-        return self._get_the_days_items(date.today() + timedelta(days=1))
+    def get_tomorrows_items(self, item_id: int = 965, limit: int = 0) -> List[str]:
+        return self._get_the_days_items(
+            date.today() + timedelta(days=1), item_id, limit
+        )
 
-    def _get_the_days_items(self, anchor: date) -> List[str]:
-        items = self.get_items()["data"]
+    def get_the_years_items(
+        self, item_id: int = 965, limit: int = 0
+    ) -> DefaultDict[date, List[str]]:
+        today = date.today()
+        start_of_year = date(today.year, 1, 1)
+        end_of_year = date(today.year, 12, 31)
+        items = self._get_items_with_dates(start_of_year, end_of_year, item_id, limit)
+
+        return items
+
+    def _get_the_days_items(self, anchor: date, item_id: int, limit: int) -> List[str]:
+        start_of_month = date.today().replace(day=1)
+        next_month = date.today().replace(day=28) + timedelta(days=4)
+        end_of_month = next_month - timedelta(days=next_month.day)
+        items = self.get_items(start_of_month, end_of_month, item_id, limit)["data"]
         item_list = [
             item["title"]
             for item in items
@@ -63,15 +70,39 @@ class GlenRockNjCalendarParser:
         ]
         return self._maybe_do_area_filter(item_list)
 
+    def _get_items_with_dates(
+        self, start_date: date, end_date: date, item_id: int, limit: int
+    ) -> DefaultDict[date, List[str]]:
+        items = self.get_items(start_date, end_date, item_id, limit)
+        items = items["data"]
+
+        result = defaultdict(list)
+        for item in items:
+            item_date = parser.parse(item["start"]).date()
+            item_title = self._maybe_do_area_filter_str(item["title"])
+            if item_title:
+                result[item_date].append(item_title)
+
+        return result
+
     def _maybe_do_area_filter(self, items: List[str]) -> List[str]:
-        if not self.area_filter:
+        if not self.item_title_filter:
             return items
 
         return [
             item
             for item in items
-            if any(match in item.lower() for match in self.area_filter)
+            if any(match in item.lower() for match in self.item_title_filter)
         ]
+
+    def _maybe_do_area_filter_str(self, item: str) -> Optional[str]:
+        if not self.item_title_filter:
+            return item
+
+        if any(match in item.lower() for match in self.item_title_filter):
+            return item
+        else:
+            return None
 
     def _parse_result_to_json(self, result: requests.Response):
         """
--- tests/test_grcalparser.py
+++ tests/test_grcalparser.py
@@ -2,10 +2,12 @@ from datetime import date
 
 from utils.grcalparser import GlenRockNjCalendarParser
 
-class TestGrCalParser:
 
+class TestGrCalParser:
     def test_basic_get(self):
-        client = GlenRockNjCalendarParser(date(2023, 1, 1), date(2023, 1, 31), area_filter=["south/east", "boroughwide"])
-        result = client.get_todays_items()
+        client = GlenRockNjCalendarParser(
+            item_title_filter=["south/east", "boroughwide"],
+        )
+        result = client.get_the_years_items()
 
         assert False
