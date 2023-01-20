5a097260057f10c6ecf3caf1a9dd4fb4d67fcb3f
Khoa Nguyen <yurameki@gmail.com>
2023-01-20 06:29:59 +0000

    Add day anchor and filter

--- src/utils/grcalparser.py
+++ src/utils/grcalparser.py
@@ -1,8 +1,9 @@
 from collections import OrderedDict
-from datetime import date
-from typing import Callable, Dict, Optional
+from datetime import date, timedelta
+from typing import Callable, Dict, List, Optional
 
 import requests
+from dateutil import parser
 
 
 class GlenRockNjCalendarParser:
@@ -16,11 +17,14 @@ class GlenRockNjCalendarParser:
         item_id: int = 965,
         limit: int = 0,
         session: Optional[requests.Session] = None,
+        area_filter: Optional[List[str]] = None,
     ) -> None:
         self.start_date = start_date
         self.end_date = end_date
         self.item_id = item_id
         self.limit = limit
+
+        self.area_filter = area_filter
 
         self.headers = {
             "Content-Type": "application/json",
@@ -30,7 +34,7 @@ class GlenRockNjCalendarParser:
         self.session: requests.Session = session or requests.Session()
         self.base_api = "https://glenrocknj.net/index.php"
 
-    def get_items(self) -> Dict[str, object]:
+    def get_items(self) -> OrderedDict:
         query_params: Dict[str, str] = {}
         query_params["option"] = "com_dpcalendar"
         query_params["view"] = "events"
@@ -42,8 +46,32 @@ class GlenRockNjCalendarParser:
 
         result = self._call_api("GET", self.base_api, params=query_params)
         result_json = self._parse_result_to_json(result)
+        return result_json
 
-        assert True
+    def get_todays_items(self) -> List[str]:
+        return self._get_the_days_items(date.today())
+
+    def get_tomorrows_items(self) -> List[str]:
+        return self._get_the_days_items(date.today() + timedelta(days=1))
+
+    def _get_the_days_items(self, anchor: date) -> List[str]:
+        items = self.get_items()["data"]
+        item_list = [
+            item["title"]
+            for item in items
+            if parser.parse(item["start"]).date() == anchor
+        ]
+        return self._maybe_do_area_filter(item_list)
+
+    def _maybe_do_area_filter(self, items: List[str]) -> List[str]:
+        if not self.area_filter:
+            return items
+
+        return [
+            item
+            for item in items
+            if any(match in item.lower() for match in self.area_filter)
+        ]
 
     def _parse_result_to_json(self, result: requests.Response):
         """
--- tests/test_grcalparser.py
+++ tests/test_grcalparser.py
@@ -5,7 +5,7 @@ from utils.grcalparser import GlenRockNjCalendarParser
 class TestGrCalParser:
 
     def test_basic_get(self):
-        client = GlenRockNjCalendarParser(date(2023, 1, 1), date(2023, 1, 31))
-        result = client.get_items()
+        client = GlenRockNjCalendarParser(date(2023, 1, 1), date(2023, 1, 31), area_filter=["south/east", "boroughwide"])
+        result = client.get_todays_items()
 
         assert False
